ce554272db6117bc4c564e28115eb7cb3f39bcc8
Krzysztof U <krzychu222u@wp.pl>
2020-03-07 09:32:04 +0100

    connection API to website/correcting design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Weather from "./components/Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "weather-icons/css/weather-icons.css";
-import {handleClick} from "./components/Weather"
+import {handleClick} from "./components/Weather";
+import Fetcher from "./components/Fetcher";
+
 
 //http://api.openweathermap.org/data/2.5/weather?q=Strzelin&appid=87342d5fe25c36f18893da03fd123f18
+
+
 const App=()=> {
   
-  const [data,setData] = useState(null)
+  const [data,setData] = useState("")
+  const [error,setError] = useState(false)
   const [city,setCity] = useState("")
- 
+  const [country,setCountry] = useState("")
+
+  useEffect(() => {
+      const APIKEY ="3685edcdeb2b60f9ec66ee97605d3489"
+  
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&APPID=${APIKEY}`
+    async function getData(){
+      const data = await Fetcher(url)
+      setData(data.data)
+    }
+    getData()
+      
+      
+    
+  
+  
+  
+   }, []);
+
+  
+
   return (
     <React.Fragment>
         <div className="App">
         <div className="App__settings">
         <h4 className="App__settings-title">Weather</h4>
         <div className="App__options">
-          <input type="text" name="weather" className="App__options-data" 
+         
+          <input type="text" name="city" className={error?"App__options-data App__options-data--error":"App__options-data"} 
           placeholder="Type your City" 
           value={city}
           onChange={(e)=>setCity(e.target.value)}
           />
-        <button onClick={()=>handleClick(setData,city)} className="App__options-btn">Search</button>
+            <input type="text" name="country" className={error?"App__options-data App__options-data--error":"App__options-data"}
+          placeholder="Type your Country" 
+          value={country}
+          onChange={(e)=>setCountry(e.target.value)}
+          />
+        <button onClick={()=>handleClick(setData,city,country,setError,setCity,setCountry)} className={error?"App__options-btn App__options-btn--error":"App__options-btn" }>Search</button>
         </div>
         </div>
         <Weather data={data}/>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,72 +1,99 @@
 import React,{useState} from 'react';
 import Fetcher from "./Fetcher"
 
-export const handleClick = async(setData,city) =>{
-  
-    const APIKEY ="3685edcdeb2b60f9ec66ee97605d3489"
+export const handleClick = async(setData,city,country,setError,setCity,setCounter) =>{
+  const APIKEY ="3685edcdeb2b60f9ec66ee97605d3489"
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${APIKEY}`
-    //https://samples.openweathermap.org/data/2.5/forecast/daily?q=M%C3%BCnchen,DE&appid=3685edcdeb2b60f9ec66ee97605d3489
-    const data = await Fetcher(url)
-    //87342d5fe25c36f18893da03fd123f18
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`;
+
+    
+  if(city ===""|| country === ""){
+    setError(true)
+  }
+  else{
+
+ const data = await Fetcher(url)
+  setData(data.data)
+  setCity("");
+  setCounter("")
+  setError(false)
+  }
+    
+    
+   
+    
   
-    setData(data.data)
+   
 }
 
-const showTemperature=(min,max) =>{
-  return(
-    <>
-        <h3>{min} &deg;</h3>
-        <h3>{max} &deg;</h3>
-    </>
-  )
+
+const getDate = ()=>{
+const months = ["styczeń","luty","marzec","kwiecień","maj","czerwiec","lipiec","sierpień","wrzesień","październik","listopad","grudzień"]
+
+  const date = new Date();
+const day = date.getDay()+1;
+const month = date.getMonth();
+
+
+return `${day} ${months[month]}`
 }
 
-const ShowWeatherContent = (data)=>{
+const getDegree = (temp)=>{
   
+  const temperatue =  temp;
+  const celcius = Math.floor(temperatue -273.15);
+
+  
+  return celcius
+}
+const ShowWeatherContent =  (data)=>{
+  const dataApi = data.main===undefined
+
   return(
     <>
-    <h5 className="weather-content__date">March 12</h5>
-    <h1 className="weather-content__month">Warsaw</h1>
+    
+      
+     
+       
+         
+  <h5 className="weather-content__date">{getDate()}</h5>
+    <h1 className="weather-content__month">{data.name} <span className="fas fa-arrow-right weather-content__month-icon"></span></h1>
     <div className="weather-content__details">
       <div className="weather-content__details-main">
-       <p className="weather-content__details-main_temp"> 4 &deg;</p>
-       <span className="weather-content__details-main-feel">feels like 7&deg;</span>
+       <p className="weather-content__details-main_temp">{dataApi?"...":getDegree(data.main.temp)}&deg;</p>
+       <span className="weather-content__details-main-feel">feels like {dataApi?"...":getDegree(data.main.temp_min)}&deg;</span>
         </div>
       <div className="weather-content__details-info">
         <div className="weather-content__details-info_element">
-          <p className="weather-content__details-info_state"><span className="wi wi-day-rain weather-content__details-info_icon"></span>Rain</p>
-          <span className="weather-content__details-info_value">40%</span>
+          <p className="weather-content__details-info_state"><span className="wi wi-cloudy weather-content__details-info_icon"></span>Cloudy</p>
+          <span className="weather-content__details-info_value">{dataApi?"...":data.clouds.all} %</span>
         </div>
         <div className="weather-content__details-info_element">
         <p className="weather-content__details-info_state"><span className="wi wi-raindrop weather-content__details-info_icon"></span>Rain</p>
           <span className="weather-content__details-info_value">40%</span>
         </div>
         <div className="weather-content__details-info_element">
-        <p className="weather-content__details-info_state"><span className="wi wi-windy weather-content__details-info_icon"></span>Rain</p>
-          <span className="weather-content__details-info_value">40%</span>
+        <p className="weather-content__details-info_state"><span className="wi wi-windy weather-content__details-info_icon"></span>Wind</p>
+  <span className="weather-content__details-info_value">{dataApi?"...":data.wind.speed} km/h</span>
         </div>
         <div className="weather-content__details-info_element">
-        <p className="weather-content__details-info_state"><span className="wi wi-day-sunny weather-content__details-info_icon"></span>Rain</p>
-          <span className="weather-content__details-info_value">40%</span>
+        <p className="weather-content__details-info_state"><span className="wi wi-humidity weather-content__details-info_icon"></span>Rain</p>
+          <span className="weather-content__details-info_value">{dataApi?"...":data.main.humidity}%</span>
         </div>
       </div>
     </div>
-    </>
+        
+        </>
+      
+    
+    
+    
+   
   )
 }
 const Weather=(props)=> {
 
-
-    
-
-
-
-
-
-
-   
-    return (
+return (
       <>
        
         <div className="weather-content">
